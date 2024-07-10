4be59b489766fe3890dae05c35cfcf57e7b99316
youssef bat <youssef.bat@students.salle.url.edu>
2024-07-10 12:28:02 +0200

    july 2024 updates

--- js/skills.js
+++ js/skills.js
@@ -1,17 +1,31 @@
 $(document).ready(function() {
-    var skills = [
-        { name: "Java", progress: 95, remaining: 5 },
-        { name: "C", progress: 85, remaining: 15 },
-        { name: "Python", progress: 75, remaining: 25 },
-        { name: "Android Studio (Java)", progress: 90, remaining: 10 },
-        { name: "SQL", progress: 90, remaining: 10 },
-        { name: "PHP", progress: 70, remaining: 30 },
-        { name: "HTML", progress: 95, remaining: 5 },
-        { name: "CSS", progress: 85, remaining: 15 },
-        { name: "JS", progress: 60, remaining: 40 },
-        { name: "Swift", progress: 70, remaining: 30 },
-        { name: "Assembly", progress: 70, remaining: 30 },
-
+    const skills = [
+        {name: "Java", progress: 95, remaining: 5},
+        {name: "C", progress: 85, remaining: 15},
+        {name: "Python", progress: 75, remaining: 25},
+        {name: "Android Studio (Java)", progress: 90, remaining: 10},
+        {name: "SQL databases", progress: 90, remaining: 10},
+        {name: "NoSQL databases", progress: 75, remaining: 25},
+        {name: "PHP", progress: 70, remaining: 30},
+        {name: "HTML", progress: 95, remaining: 5},
+        {name: "CSS", progress: 85, remaining: 15},
+        {name: "JS", progress: 60, remaining: 40},
+        {name: "Swift", progress: 70, remaining: 30},
+        {name: "Assembly", progress: 70, remaining: 30},
+        {name: "Git", progress: 95, remaining: 5},
+        {name: "Flutter", progress: 70, remaining: 30},
+        {name: "Deep learning", progress: 70, remaining: 30},
+        {name: "AI", progress: 70, remaining: 30},
+        {name: "CV", progress: 70, remaining: 30},
+        {name: "ML", progress: 70, remaining: 30},
+        {name: "Cloud computing", progress: 70, remaining: 30},
+        {name: "Quality testing", progress: 80, remaining: 20},
+        {name: "OS", progress: 80, remaining: 20},
+        {name: "Advanced analytics", progress: 80, remaining: 20},
+        {name: "Agile methodologies", progress: 100, remaining: 0},
+        {name: "Adobe illustrator", progress: 70, remaining: 30},
+        {name: "Canva", progress: 90, remaining: 10},
+        {name: "Figma", progress: 90, remaining: 10}
     ];
 
     var chartConfig = {
